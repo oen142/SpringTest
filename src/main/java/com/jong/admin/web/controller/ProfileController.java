cb04a75a6c0e5ceb5674dbc7e0ec28f1abccffc8
kim jong wan <oen142@naver.com>
2020-04-23 19:51:24 +0900

    test

--- src/main/java/com/jong/admin/web/controller/ProfileController.java
+++ src/main/java/com/jong/admin/web/controller/ProfileController.java
@@ -11,12 +11,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
-    private final Environment environment;
+    private final Environment env;
 
     @GetMapping("/profile")
-    public String profile(){
-        List<String> profiles= Arrays.asList(environment.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real" , "real1" , "real2");
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
         return profiles.stream()
--- src/test/java/com/jong/admin/web/ProfileControllerUnitTest.java
+++ src/test/java/com/jong/admin/web/ProfileControllerUnitTest.java
@@ -6,8 +6,10 @@ import org.springframework.mock.env.MockEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class ProfileControllerUnitTest {
+
     @Test
-    public void search_real_profile(){
+    public void real_profile이_조회된다() {
+        //given
         String expectedProfile = "real";
         MockEnvironment env = new MockEnvironment();
         env.addActiveProfile(expectedProfile);
@@ -16,12 +18,13 @@ public class ProfileControllerUnitTest {
 
         ProfileController controller = new ProfileController(env);
 
+        //when
         String profile = controller.profile();
 
+        //then
         assertThat(profile).isEqualTo(expectedProfile);
-
-
     }
+
     @Test
     public void real_profile이_없으면_첫번째가_조회된다() {
         //given
