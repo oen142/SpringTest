5fda5d16a97160c1b5d011085c379e37e6b47319
kim jong wan <oen142@naver.com>
2020-04-28 15:21:33 +0900

    test

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,26 +1,16 @@
-#!/bin/bash
-echo "> 현재 구동중인 Port 확인"
-CURRENT_PROFILE=$(curl -s http://localhost/profile)
+#!/usr/bin/env bash
 
-# 쉬고 있는 set 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
-if [ $CURRENT_PROFILE == set1 ]
-then
-  IDLE_PORT=8082
-elif [ $CURRENT_PROFILE == set2 ]
-then
-  IDLE_PORT=8081
-else
-  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
-  echo "> 8081을 할당합니다."
-  IDLE_PORT=8081
-fi
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-echo "> 전환할 Port: $IDLE_PORT"
-echo "> Port 전환"
-echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
+function switch_proxy() {
+ IDLE_PORT=$(find_idle_port)
 
-PROXY_PORT=$(curl -s http://localhost/profile)
-echo "> Nginx Current Proxy Port: $PROXY_PORT"
+ echo "> 전환할 Port: $IDLE_PORT"
+ echo "> Port 전환"
+ echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-echo "> Nginx Reload"
-sudo service nginx reload
+ echo "> 엔진엑스 Reload"
+ sudo service nginx reload
+}
